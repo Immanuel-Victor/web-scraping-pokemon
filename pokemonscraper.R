67b4f40ffb653dc1c0f8f64e24d48f03125a27e4
immanuel <immanuel.victor@academico.ifpb.edu.br>
2022-04-24 16:32:25 -0300

    feat: torna o script capaz de abrir o navegador e navegar ate a pagina

--- pokemonscraper.R
+++ pokemonscraper.R
@@ -1,12 +1,22 @@
 # Instala os pacotes que vamos usar nesse script
-install.packages(c("RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
+install.packages(c("netstat","RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
 
 # Carrega os pacotes para uso
 
-Packages <- c("RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
+Packages <- c("netstat","RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
 lapply(Packages, library, character.only = TRUE)
 
 # Começo do Script -----------------------------------------------------------------------------------------------------------------------------------
 
-RSelenium::rsDriver(browser = "firefox")
+#Usando firefox por que o Chrome acabou não funcionando
+rs_driver_object<- rsDriver(
+  browser = "firefox",
+  geckover = "0.31.0",
+  verbose = F,
+)
 
+remDr <- rs_driver_object$client
+
+remDr$open()
+remDr$navigate("https://www.pokemon.com/br/pokedex/")
+#Use rsDriver() para usar o chrome como padrão ou rsDriver(browser = "internet explorer") para o internet explorer
