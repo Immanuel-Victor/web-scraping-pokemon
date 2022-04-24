78e42af7f82d8e608b9d599fbe9832554414092b
immanuel <immanuel.victor@academico.ifpb.edu.br>
2022-04-24 15:20:43 -0300

    refactor: adiciona uma livraria a mais

--- pokemonscraper.R
+++ pokemonscraper.R
@@ -1,12 +1,12 @@
 # Instala os pacotes que vamos usar nesse script
-install.packages(c("tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
+install.packages(c("Rselenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
 
 # Carrega os pacotes para uso
 
-Packages <- c("tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
+Packages <- c("Rselenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
 lapply(Packages, library, character.only = TRUE)
 
 # Começo do Script -----------------------------------------------------------------------------------------------------------------------------------
 
-pokedex-website <- read_html("https://www.pokemon.com/br/pokedex/")
+pokedex_website <- read_html("https://www.pokemon.com/br/pokedex/")
 
