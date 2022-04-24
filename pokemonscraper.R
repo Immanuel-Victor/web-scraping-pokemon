1a8058513a7d6974b2e7698fed2f3f81e934391e
immanuel <immanuel.victor@academico.ifpb.edu.br>
2022-04-24 16:31:31 -0300

    env: prepara o ambiente para usar o RSelenium

--- pokemonscraper.R
+++ pokemonscraper.R
@@ -1,12 +1,12 @@
 # Instala os pacotes que vamos usar nesse script
-install.packages(c("Rselenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
+install.packages(c("RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate"))
 
 # Carrega os pacotes para uso
 
-Packages <- c("Rselenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
+Packages <- c("RSelenium","tidyverse", "rvest", "stringr", "LSAfun", "lubridate")
 lapply(Packages, library, character.only = TRUE)
 
 # Começo do Script -----------------------------------------------------------------------------------------------------------------------------------
 
-pokedex_website <- read_html("https://www.pokemon.com/br/pokedex/")
+RSelenium::rsDriver(browser = "firefox")
 
